48aac53832dc9e13bb4f78ea558df2d2a5cdb044
marcosdarri <marcosdarri@hotmail.com>
2023-10-09 12:13:35 -0300

    Adds margin to the grid.

--- app/components/Grid/Grid.js
+++ app/components/Grid/Grid.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Grid = ({ children }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-8 lg:mx-20">
       {children}
     </div>
   );
--- app/components/PageTitle/PageTitle.js
+++ app/components/PageTitle/PageTitle.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PageTitle = ({totalSum}) => {
   return (
     <div className="mb-5 w-full">
-    <h1 className="text-black text-4xl font-bold">Total sum: {totalSum}</h1>
+    <h1 className="text-black text-4xl font-bold lg:mx-20">Total sum: {totalSum}</h1>
   </div>
   )
 }
